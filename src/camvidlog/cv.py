c32c1739ea99d72d724dcc414dcd285df24b16a2
Adam Faulconbridge <afaulconbridge@googlemail.com>
2023-12-01 15:12:35 +0000

    minor cleanup

--- src/camvidlog/cv.py
+++ src/camvidlog/cv.py
@@ -26,7 +26,7 @@ class ComputerVisionService:
     def __init__(self):
         self.model = YOLO(get_data("yolov8x-oiv7.pt"))
 
-    def analyse_video(self, videopath, framestep=3) -> tuple[VideoResult, ...]:
+    def analyse_video(self, videopath, framestep=1) -> tuple[VideoResult, ...]:
         tracks = {}
         # iou = "intersection over union" for non-maximum supression (NMS)
         # tl;dr remove overlapping hits, lower is stricter
@@ -44,18 +44,15 @@ class ComputerVisionService:
                 box_conf = box.conf.item()
                 box_cls = box.cls.item()
                 print(f"{box_id} {box_cls} {box_conf}")
+
                 if box_id in tracks:
                     previous_result = tracks[box_id]
                     tracks[box_id] = VideoResult(
                         timestart=previous_result.timestart,
                         timeend=i,
-                        probs=(),
+                        probs=(*previous_result.probs, (box_cls, box_conf)),
                     )
-                elif i == 10:  # noqa: PLR2004
-                    # tracking algorithm takes 10 images to get a good lock
-                    # so assume its been there from the start if this is the 10th step
-                    tracks[box_id] = VideoResult(timestart=0, timeend=i, probs=())
                 else:
-                    tracks[box_id] = VideoResult(timestart=i, timeend=i, probs=())
+                    tracks[box_id] = VideoResult(timestart=i, timeend=i, probs=((box_cls, box_conf),))
 
         return tuple(tracks.values())
