4d6c7ebcae288de7ad2b4dfd2e6a6538e83223c0
Adam Faulconbridge <afaulconbridge@googlemail.com>
2024-01-01 16:40:15 +0000

    add pagesize

--- src/camvidlog/app.py
+++ src/camvidlog/app.py
@@ -1,4 +1,5 @@
 import base64
+import itertools
 from pathlib import Path
 from typing import Any
 
@@ -10,6 +11,10 @@ from camvidlog.db.service import DbService
 # Part 1: ui ----
 app_ui = shiny.ui.page_fluid(
     shiny.ui.panel_title("CamVidLog", "CamVidLog"),
+    shiny.ui.div(
+        shiny.ui.input_selectize("page_size", "Page Size", {5: "5", 10: "10", 25: "25", 50: "50"}, selected="5"),
+    ),
+    shiny.ui.tags.hr(),
     shiny.ui.output_ui("videos"),
 )
 
@@ -19,10 +24,22 @@ db_service = DbService(config.database_url)
 
 # Part 2: server ----
 def server(input: Any, output: Any, session: Any) -> Any:  # noqa: A002, ARG001
+    @shiny.reactive.Calc
+    def get_records():
+        page_size = int(input.page_size())
+        page_offset = 0
+        return tuple(
+            itertools.islice(
+                db_service.get_tracks_with_videos(),
+                page_size * page_offset,
+                page_size * (page_offset + 1),
+            )
+        )
+
     @output
     @shiny.render.ui
     def videos():
-        records = db_service.get_tracks_with_videos()
+        records = get_records()
         # filename
         # frame from-to
         results = []
@@ -35,7 +52,9 @@ def server(input: Any, output: Any, session: Any) -> Any:  # noqa: A002, ARG001
                 result_parts.extend(
                     (
                         shiny.ui.div(
-                            f"{track.frame_first:04d}-{track.frame_last:04d} ({track.frame_last-track.frame_first+1})"
+                            shiny.ui.p(
+                                f"{track.frame_first:04d}-{track.frame_last:04d} ({track.frame_last-track.frame_first+1})"
+                            )
                         ),
                         shiny.ui.div(
                             shiny.ui.img(
@@ -51,6 +70,7 @@ def server(input: Any, output: Any, session: Any) -> Any:  # noqa: A002, ARG001
                     )
                 )
 
+            result_parts.append(shiny.ui.tags.hr())
             results.append(shiny.ui.div(*result_parts))
 
         return shiny.ui.page_fluid(*results)
