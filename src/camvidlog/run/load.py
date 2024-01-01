424cd625a54ab5042d04b3f7ae6a2e98672a2b3a
Adam Faulconbridge <afaulconbridge@googlemail.com>
2024-01-01 16:04:55 +0000

    cv2 thumbnails with contrast

--- src/camvidlog/run/load.py
+++ src/camvidlog/run/load.py
@@ -1,10 +1,11 @@
 import argparse
 import logging
-from io import BytesIO
+import math
 from pathlib import Path
 
+import cv2
+import numpy as np
 from numpy import ndarray
-from PIL import Image, ImageOps
 
 from camvidlog.config import ConfigService
 from camvidlog.cv.service import ComputerVisionService
@@ -13,13 +14,71 @@ from camvidlog.db.service import DbService
 logger = logging.getLogger(__name__)
 
 
-def make_thumbnail(source: ndarray, size=(120, 120)) -> bytes:
-    image = Image.fromarray(source)
-    image.thumbnail(size)
-    image = ImageOps.pad(image, size)
-    with BytesIO() as tmpfile:
-        image.save(tmpfile, format="jpeg")
-        return tmpfile.getvalue()
+def cv2_resize(image: ndarray, width: int, height: int, inter: int = cv2.INTER_AREA, border: int = 0):
+    # based on https://github.com/PyImageSearch/imutils
+
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    (h, w) = image.shape[:2]
+    if w > h:
+        # calculate the ratio of the width and construct the dimensions
+        r = width / w
+        dim = (int(width), int(h * r))
+    else:
+        # calculate the ratio of the height and construct the dimensions
+        r = height / h
+        dim = (int(w * r), int(height))
+
+    # resize the image
+    # if making it smaller, use cv2.INTER_AREA
+    # if making it larger, use cv2.INTER_CUBIC
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # apply a border to get real size
+    height_resized, width_resized, *_ = resized.shape
+    border_left = math.floor((width - width_resized) / 2)
+    border_right = math.ceil((width - width_resized) / 2)
+    border_top = math.floor((height - height_resized) / 2)
+    border_bottom = math.ceil((height - height_resized) / 2)
+    if border_left or border_right or border_top or border_bottom:
+        bordered = cv2.copyMakeBorder(
+            resized, border_top, border_bottom, border_left, border_right, cv2.BORDER_CONSTANT, value=border
+        )
+        return bordered
+    else:
+        # no border needed
+        return resized
+
+
+def make_thumbnail(image: ndarray, size=(120, 120)) -> bytes:
+    # need to be converted to grayscale before contrast manipulation
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # global histogram equalization
+    image = cv2.equalizeHist(image)
+
+    # local histogram equalization
+    # clip = 1
+    # tile = 4
+    # clahe = cv2.createCLAHE(clipLimit=clip, tileGridSize=(tile, tile))
+    # image = clahe.apply(image)
+
+    # resize and border
+    # do this _after_ histogram normalization so that any border is not included in that step
+    # if its making it smaller, use cv2.INTER_AREA
+    # if its making it larger, use cv2.INTER_CUBIC
+    if image.shape[0] < size[0] and image.shape[0] < size[1]:
+        inter = cv2.INTER_AREA
+    else:
+        inter = cv2.INTER_CUBIC
+    image = cv2_resize(image, size[0], size[1], inter)
+
+    # turn image into jpeg bytes
+    img_encode = cv2.imencode(".jpg", image)[1]
+    data_encode = np.array(img_encode)
+    byte_encode = data_encode.tobytes()
+
+    return byte_encode
 
 
 if __name__ == "__main__":
@@ -40,7 +99,7 @@ if __name__ == "__main__":
 
         video_path = Path(filename).resolve()
 
-        tracks = cv_service.find_things(video_path)
+        tracks = cv_service.find_things(video_path, framestep=3)
         logger.info(f"Found {len(tracks)} tracks")
 
         db_service.add_video(filename=filename)
