20babdaf3dd870684715d620ac2acc43d2760e0e
Adam Faulconbridge <afaulconbridge@googlemail.com>
2024-06-06 12:24:00 +0000

    grounding dino initial

---
+++ experimental/10_grounding_dino/run.py
@@ -0,0 +1,106 @@
+from contextlib import contextmanager
+import time
+import ffmpeg
+import torch
+from PIL import Image, ImageDraw
+from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def timectx(description: str):
+    start = time.time()
+    yield
+    end = time.time()
+    logger.info(f"{description} = {(end-start)*1000:.2f}ms")
+
+
+model_id = "IDEA-Research/grounding-dino-base"
+device = "cuda"
+
+# ffmpeg_src = "rtsp://192.168.1.104:8554/cam1_sub"
+ffmpeg_src = "file:///workspaces/camvidlog/data/20231117215252_VD_00328.MP4"
+ffmpeg_dst = "file:///workspaces/camvidlog/out.MP4"
+text_queries = ["screenshot from trail camera video footage", "person", "cat", "deer", "bird"]
+
+logger.info("setup started")
+
+processor = AutoProcessor.from_pretrained(model_id)
+model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
+
+probe = ffmpeg.probe(ffmpeg_src)
+video_stream = next(
+    (stream for stream in probe["streams"] if stream["codec_type"] == "video"),
+    None,
+)
+width = int(video_stream["width"])
+height = int(video_stream["height"])
+
+# -hwaccel cuda -hwaccel_output_format cuda
+# -hwaccel qsv -c:v h264_qsv
+
+
+reader = (
+    ffmpeg.input(ffmpeg_src)
+    .output("pipe:", format="rawvideo", pix_fmt="rgb24")
+    .run_async(pipe_stdout=True, quiet=True)
+)
+writer = (
+    ffmpeg.input(
+        "pipe:",
+        format="rawvideo",
+        pix_fmt="rgb24",
+        s=f"{width}x{height}",
+    )
+    .output(
+        ffmpeg_dst,
+        pix_fmt="yuv420p",
+    )
+    .overwrite_output()
+    .run_async(
+        pipe_stdin=True,
+        quiet=True,
+    )
+)
+
+logger.info("setup complete")
+i = -1
+while True:
+    i += 1
+
+    with timectx(f"frame {i} read"):
+        in_bytes = reader.stdout.read(width * height * 3)
+        if len(in_bytes) != width * height * 3:
+            break
+        image = Image.frombytes("RGB", (width, height), in_bytes)
+
+    if i % 30 == 0:
+        with timectx(f"frame {i} process"):
+            inputs = processor(images=image, text=". ".join(text_queries), return_tensors="pt").to(device)
+            with torch.no_grad():
+                outputs = model(**inputs)
+                results = processor.post_process_grounded_object_detection(
+                    outputs, inputs.input_ids, box_threshold=0.5, text_threshold=0.5, target_sizes=[image.size[::-1]]
+                )[0]
+
+    with timectx(f"frame {i} write"):
+        draw = ImageDraw.Draw(image)
+        for j, (score, label, bbox) in enumerate(zip(*results.values())):
+            if label.startswith("screens"):
+                # screens or screenshot ?
+                # skip the context label
+                continue
+            logging.info(f"{i} {j} {score} {label}")
+            xmin, ymin, xmax, ymax = bbox
+            draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
+            draw.text((xmin, ymin), f"{label} {score:0.2f}", fill="white")
+        writer.stdin.write(image.tobytes())
+
+writer.stdin.close()
+writer.wait()
+
+for line in reader.stderr.readlines():
+    print(line)
---
+++ src/camvidlog/run/scan.py
@@ -0,0 +1,140 @@
+import argparse
+import json
+import logging
+from pathlib import Path
+from typing import Generator, Iterable, Optional
+
+import cv2
+import torch
+from PIL import Image
+from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class VideoManager:
+    videopath: Path | str
+    fps: Optional[float] = None
+    capture: Optional[cv2.VideoCapture]
+
+    def __init__(self, videopath: Path | str):
+        self.videopath = videopath
+
+    def __enter__(self):
+        self.capture = cv2.VideoCapture(str(self.videopath), cv2.CAP_ANY)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.capture.release()
+        self.capture = None
+        self.fps = None
+
+    def generate_frames(self) -> Generator[tuple[int, float, cv2.typing.MatLike], None, None]:
+        frame_no = 0
+        frame_time = 0.0  # time since start in seconds
+        # get first frame
+        sucess, frame = self.capture.read()
+        # TODO verify RGB?
+
+        if not self.fps:
+            self.fps = float(self.capture.get(cv2.CAP_PROP_FPS))
+
+        while sucess:
+            # handle frame
+            yield frame_no, frame_time, frame
+
+            # get next frame
+            sucess, frame = self.capture.read()
+            frame_no += 1
+            frame_time += 1.0 / self.fps
+
+
+class ImageManager:
+    model_id: str
+    text_queries: str
+    threshold_box: float
+    threshold_text: float
+
+    def __init__(
+        self,
+        queries: Iterable[str],
+        model_id="IDEA-Research/grounding-dino-base",
+        threshold_box: float = 0.25,
+        threshold_text: float = 0.25,
+    ):
+        self.model_id = model_id
+        self.text_queries = ". ".join(queries) + ". "
+        self.threshold_box = threshold_box
+        self.threshold_text = threshold_text
+
+    def __enter__(self):
+        logger.debug("Creating Autoprocessor")
+        self.processor = AutoProcessor.from_pretrained(self.model_id)
+        logger.debug("Creating AutoModelForZeroShotObjectDetection")
+        self.model = AutoModelForZeroShotObjectDetection.from_pretrained(self.model_id).to("cuda")
+        return self
+
+    def __exit__(self, type, value, traceback):
+        pass
+
+    def process(self, image: cv2.typing.MatLike) -> None:
+        image_pillow = Image.fromarray(image)
+        inputs = self.processor(images=image_pillow, text=self.text_queries, return_tensors="pt").to("cuda")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            results = self.processor.post_process_grounded_object_detection(
+                outputs,
+                inputs.input_ids,
+                box_threshold=self.threshold_box,
+                text_threshold=self.threshold_text,
+                target_sizes=[image_pillow.size[::-1]],
+            )[0]
+        for score, label, bbox in zip(*results.values()):
+            yield float(score), str(label), [int(i) for i in bbox]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="CamVidLogScan", description="Scans video files to find things")
+    parser.add_argument("filename", nargs="+")
+    args = parser.parse_args()
+
+    # hedgehog - very hard for ML!
+    # data/20231119173624_VD_00333.MP4
+
+    queries = "person", "cat", "deer", "bird", "otter", "fox", "hedgehog", "mouse", "rat"
+
+    for filename in args.filename:
+        output = str(Path(filename).with_suffix("").with_suffix(".json"))
+
+        results = {}
+        result_frames = []
+        results["frames"] = result_frames
+
+        with ImageManager(queries) as imagemanager:
+            with VideoManager(filename) as videomanager:
+                process_time = 0.0
+                process_interval = 1.0
+                for i, time, frame in videomanager.generate_frames():
+                    if "fps" not in results:
+                        results["fps"] = videomanager.fps
+
+                    if time >= (process_time - 0.00000001):  # handle float point error
+                        logger.debug(f"process {i} {time}")
+                        for score, label, bbox in imagemanager.process(frame):
+                            result_frame = {}
+                            result_frame["number"] = i
+                            result_frame["time"] = time
+                            result_frame["score"] = score
+                            result_frame["label"] = label
+                            result_frame["bbox"] = bbox
+                            result_frame["model"] = imagemanager.model_id
+                            result_frame["query"] = imagemanager.text_queries
+                            result_frames.append(result_frame)
+                        # move to next slot
+                        process_time += process_interval
+
+        # TODO merge with existing file if it exists
+        with open(output, "w") as outfile:
+            json.dump(results, outfile, indent=2, sort_keys=True)
