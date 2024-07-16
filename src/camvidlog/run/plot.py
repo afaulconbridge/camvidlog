becdd88e8cf9eb7a80b7b307928d01cd71e5d022
Adam Faulconbridge <afaulconbridge@googlemail.com>
2024-07-16 18:43:35 +0000

    basic plotting

---
+++ src/camvidlog/run/plot.py
@@ -0,0 +1,18 @@
+import argparse
+
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", nargs="+")
+    args = parser.parse_args()
+    filenames: tuple[str] = tuple(args.filename)
+    for filename in filenames:
+        data = pd.read_csv(filename)
+        plot = sns.relplot(data=data, x="frame_no", y="hits.0.score", kind="line")
+        fig = plot.figure
+        fig.axes[0].set_ylim(0, 1)
+        fig.savefig("out.png")
