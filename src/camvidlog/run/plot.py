c2ce1ec1114e810f9bacedc8c1d6f7e1a6e9234b
Adam Faulconbridge <afaulconbridge@googlemail.com>
2024-07-19 22:59:55 +0000

    plot filenames

--- src/camvidlog/run/plot.py
+++ src/camvidlog/run/plot.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     filenames: tuple[str] = tuple(args.filename)
     for filename in filenames:
+        filename_out = filename[:-4] + ".png"
         data = pd.read_csv(filename)
         plot = sns.relplot(data=data, x="frame_no", y="hits.0.score", kind="line")
         fig = plot.figure
         fig.axes[0].set_ylim(0, 1)
-        fig.savefig("out.png")
+        fig.savefig(filename_out)
