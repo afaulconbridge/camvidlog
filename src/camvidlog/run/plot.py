d2c1c8cef5a90df7003625694280dc77977d179d
Adam Faulconbridge <afaulconbridge@googlemail.com>
2024-10-02 21:19:05 +0000

    cleaner plot

--- src/camvidlog/run/plot.py
+++ src/camvidlog/run/plot.py
@@ -18,15 +18,16 @@ if __name__ == "__main__":
         data_ai = pd.read_csv(filename_video + ".ai.csv")
         data_ai_grouped = data_ai.groupby(["frame_no", "label", "model_id"], as_index=False).max()
 
-        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+        # fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+        fig, ax1 = plt.subplots(nrows=1, ncols=1)
 
         sns.lineplot(data_ai_grouped, x="frame_no", y="score", style="label", hue="label", ax=ax1)
-        ax1.set_ylim(0, 1)
+        # ax1.set_ylim(0, 1)
         ax1.set_xlim(0, 900)
 
-        sns.lineplot(data, x="frame_no", y="mask.mean", style="res", hue="res", ax=ax2)
-        ax2.set_ylim(0, 0.25)
-        ax2.set_xlim(0, 900)
+        # sns.lineplot(data, x="frame_no", y="mask.mean", style="res", hue="res", ax=ax2)
+        # ax2.set_ylim(0, 0.25)
+        # ax2.set_xlim(0, 900)
 
         fig.savefig(filename_out)
         fig.clf()
